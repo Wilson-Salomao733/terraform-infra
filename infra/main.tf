651ed3fd2b12cde15f7fac81e7767a84991d52bc
Wilson Salomao <wilson.salomao733@gmail.com>
2025-07-08 11:28:03 -0300

    update terraform vars

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,11 @@
-
 resource "aws_s3_bucket" "bucket" {
     bucket = var.bucket_name
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  tags = {
+    Name = var.ec2_instance_name
+  }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,15 @@
 variable "bucket_name"{
     type = string
 }
+
+variable "ec2_ami" {
+  type = string
+}
+
+variable "ec2_instance_type" {
+  type = string
+}
+
+variable "ec2_instance_name" {
+  type = string
+}
